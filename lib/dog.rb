7189293e2f04d8aafd6728ff5bc996a3743b4c59
hersson ceballos <herssonceballos@gmail.com>
2020-08-31 01:14:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,6 @@
 # Add your code here
 class Dog 
+<<<<<<< HEAD
   @@all = []
   
   def initialize(name)
@@ -32,3 +33,54 @@ end
   
   
   
+=======
+  
+  attr_accessor :name 
+  
+  ALL = []
+  
+  def initialize(name)
+    @name = name
+    ALL = self
+ end 
+  
+  def self.all
+    All 
+  end 
+  
+  
+end 
+
+# class Dog
+#     @@all = []
+  
+#     def initialize(name)
+#       @name = name
+#       self.save
+#     end
+  
+#     def self.all
+#       @@all
+#     end
+  
+#     def self.clear_all
+#       @@all = [] 
+#     end
+  
+#     def self.print_all
+#       @@all.each do |dog|
+#         puts dog.name
+#       end
+#   end
+  
+#     def save
+#       @@all << self
+#     end
+  
+#     def name
+#       @name
+#     end
+  
+#   end
+# end
+>>>>>>> ca9ef31acfd747747f59e69eac643b6f79a3b39a
